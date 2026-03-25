f581078c65ab6c23fc3c7d36d148d0fc1c1f8c5d
GiGiKoneti <gigikoneti@gmail.com>
2026-03-25 13:04:50 +0530

    fix(sampler): support learned noise schedule override and remove latent clamp

--- src/sampler.py
+++ src/sampler.py
@@ -9,12 +9,9 @@ class DDPMSampler:
         self.max_log_snr = max_log_snr
 
     @torch.no_grad()
-    def step(self, x_t, t, t_prev, conditioning=None):
+    def step(self, x_t, t, t_prev, conditioning=None, max_log_snr_override=None):
         """
         One step of DDPM denoising.
-        x_t: (B, C, H, W) or (B, N, D)
-        t: current timestep (0 to 1)
-        t_prev: previous timestep (closer to 0)
         """
         device = x_t.device
         b = x_t.shape[0]
@@ -26,9 +23,12 @@ class DDPMSampler:
         else:
             eps_hat = self.model(x_t, t_batch)
             
-        # 2. Get alpha/sigma for current and previous
-        alpha_t, sigma_t = log_snr_to_alpha_sigma(log_snr(torch.tensor([t], device=device), self.min_log_snr, self.max_log_snr))
-        alpha_prev, sigma_prev = log_snr_to_alpha_sigma(log_snr(torch.tensor([t_prev], device=device), self.min_log_snr, self.max_log_snr))
+        # 2. Get alpha/sigma for current and previous using the SAME schedule as training
+        log_snr_t = log_snr(torch.tensor([t], device=device), self.min_log_snr, self.max_log_snr, max_log_snr_override)
+        alpha_t, sigma_t = log_snr_to_alpha_sigma(log_snr_t)
+        
+        log_snr_prev = log_snr(torch.tensor([t_prev], device=device), self.min_log_snr, self.max_log_snr, max_log_snr_override)
+        alpha_prev, sigma_prev = log_snr_to_alpha_sigma(log_snr_prev)
         
         # Reshape for broadcasting
         alpha_t = alpha_t.view(-1, *([1] * (x_t.ndim - 1)))
@@ -37,9 +37,7 @@ class DDPMSampler:
         sigma_prev = sigma_prev.view(-1, *([1] * (x_t.ndim - 1)))
 
         # 3. Predict x_0 (clean)
-        # x_t = alpha_t * x_0 + sigma_t * eps
         x_0_hat = (x_t - sigma_t * eps_hat) / alpha_t
-        x_0_hat = x_0_hat.clamp(-1, 1) # Optional but common for images
         
         # 4. Posterior mean direction (DDPM)
         x_prev = alpha_prev * x_0_hat + sigma_prev * eps_hat
@@ -47,7 +45,7 @@ class DDPMSampler:
         return x_prev
 
     @torch.no_grad()
-    def sample(self, shape, steps=50, conditioning=None, device='cpu'):
+    def sample(self, shape, steps=50, conditioning=None, device='cpu', max_log_snr_override=None):
         """
         Full sampling loop from T=1 to 0.
         """
@@ -57,6 +55,6 @@ class DDPMSampler:
         for i in range(steps):
             t = timesteps[i].item()
             t_prev = timesteps[i+1].item()
-            x = self.step(x, t, t_prev, conditioning)
+            x = self.step(x, t, t_prev, conditioning, max_log_snr_override=max_log_snr_override)
             
         return x
